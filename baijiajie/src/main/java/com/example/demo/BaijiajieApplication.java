dd63ada9112ba412a520236fd157112ce2427f78
littlehen <1036383548@qq.com>
2017-12-21 00:46:17 +0800

    1

--- baijiajie/src/main/java/com/example/demo/BaijiajieApplication.java
+++ baijiajie/src/main/java/com/example/demo/BaijiajieApplication.java
@@ -2,10 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BaijiajieApplication {
+public class BaijiajieApplication extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BaijiajieApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(BaijiajieApplication.class, args);
 	}
